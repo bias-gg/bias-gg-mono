00ab619b22b15450af39c63abdff7798a6e96236
Josh Peters <jgpeters717@gmail.com>
2025-05-02 09:05:47 -0400

    start getting ready for drizzle

--- apps/backend/routes/cards.routes.ts
+++
@@ -1,6 +0,0 @@
-import Elysia from "elysia";
-import { CardsController } from "../controllers/cards.controller";
-
-export const cardRoutes = new Elysia({ prefix: "/cards" })
-  .get("/hottest", () => CardsController.getHottest())
-  .get("/:id", ({ params }) => CardsController.getCardById(params.id));
